c66652d5427d0f556efc4e5c4c39af2cd1240604
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-18 18:22:28 -0400

    change config backend host address

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 var config = {};
 
-config.backend = 'http://localhost:5000';
+config.backend = 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com:5000';
 module.exports = config;
