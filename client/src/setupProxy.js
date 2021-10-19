528fe20e7c80dc6900e5d5e9de51b9e1676ca088
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-19 17:04:08 -0400

    adding port number

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/',
     createProxyMiddleware({
-      target: `http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com`,
+      target: `http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com:5000`,
       //target: 'http://localhost:5000',
       changeOrigin: true,
     })
