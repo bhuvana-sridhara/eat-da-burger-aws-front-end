750a35ff6784d8f853306786097cc804544e9d0a
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-19 17:00:45 -0400

    adding protocol name

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/',
     createProxyMiddleware({
-      target: 'internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
+      target: `http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com`,
       //target: 'http://localhost:5000',
       changeOrigin: true,
     })
