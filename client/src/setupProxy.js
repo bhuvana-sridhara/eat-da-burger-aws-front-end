dc213ae99d4f3b61afc382afcf8b70acd40e6919
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-19 17:26:16 -0400

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,10 +4,7 @@ module.exports = function(app) {
   app.use(
     '/',
     createProxyMiddleware({
-      target: {
-          host: 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
-          port: 5000
-      },
+      target: 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
       //target: 'http://localhost:5000',
       changeOrigin: true,
     })
