10c97e9732f3e393c179e73246a6e08553110df0
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-19 17:14:25 -0400

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
       target: {
           host: 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
           port: 5000
-      }
+      },
       //target: 'http://localhost:5000',
       changeOrigin: true,
     })
