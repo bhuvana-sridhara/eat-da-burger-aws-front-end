0812952a68bcb9eaffa1919d3600728d6e1b6382
bhuvana-sridhara <bhuvana.sridhara@gmail.com>
2021-10-19 17:57:10 -0400

    fix

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,15 +2,25 @@ const createProxyMiddleware = require('http-proxy-middleware');
 
 //app.use('/', proxy('http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com'))
 
-module.exports = function(app) {
-  app.use(
-    '/',
-    createProxyMiddleware({
-      target: 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
-      //target: 'http://localhost:5000',
-      changeOrigin: true,
-    })
-  );
+const PORT = 5000;
+const HOST = "localhost";
+const API_SERVICE_URL = "http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com";
 
-  app.listen(5000);
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/',
+//     createProxyMiddleware({
+//       target: 'http://internal-3tier-b-m-Internal-LB-1739696943.us-east-1.elb.amazonaws.com',
+//       //target: 'http://localhost:5000',
+//       changeOrigin: true,
+//     })
+//   );
+
+app.use('/', createProxyMiddleware({
+   target: API_SERVICE_URL,
+   changeOrigin: true,
+}));
+
+  app.listen(PORT, HOST, () => {
+   console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
